d13f24828d8806518a3f00bd9aee00486f7adfe7
oak <38601989@qq.com>
2020-12-09 13:54:21 +0800

    1.0.1

--- src/__tests__/util.test.ts
+++ src/__tests__/util.test.ts
@@ -1,31 +1,52 @@
 import { darkmode } from '../util'
 
 test('test', () => {
-    const originalWindow = { ...window };
-    const windowSpy = jest.spyOn(global, "window", "get");
-    windowSpy.mockImplementation(() => ({
-        ...originalWindow,
-        matchMedia: () => ({ matches: true, addListener: (fn: any) => fn({matches: true}), removeListener: () => void 0})
-    } as any))
+    const originalWindow = { ...window }
+    const windowSpy = jest.spyOn(global, 'window', 'get')
+    windowSpy.mockImplementation(
+        () =>
+            ({
+                ...originalWindow,
+                matchMedia: () => ({
+                    matches: true,
+                    addListener: (fn: any) => fn({ matches: true }),
+                    removeListener: () => void 0,
+                }),
+            } as any)
+    )
     expect(darkmode((e, k) => {}).removeListeners())
 })
 
 test('test1', () => {
-    const originalWindow = { ...window };
-    const windowSpy = jest.spyOn(global, "window", "get");
-    windowSpy.mockImplementation(() => ({
-        ...originalWindow,
-        matchMedia: () => ({ matches: true, addListener: () => void 0, removeListener: () => void 0})
-    } as any))
+    const originalWindow = { ...window }
+    const windowSpy = jest.spyOn(global, 'window', 'get')
+    windowSpy.mockImplementation(
+        () =>
+            ({
+                ...originalWindow,
+                matchMedia: () => ({
+                    matches: true,
+                    addListener: () => void 0,
+                    removeListener: () => void 0,
+                }),
+            } as any)
+    )
     expect(darkmode((e, k) => {}).removeListeners())
 })
 
 test('test2', () => {
-    const originalWindow = { ...window };
-    const windowSpy = jest.spyOn(global, "window", "get");
-    windowSpy.mockImplementation(() => ({
-        ...originalWindow,
-        matchMedia: () => ({ matches: false, addListener: () => void 0, removeListener: () => void 0})
-    } as any))
+    const originalWindow = { ...window }
+    const windowSpy = jest.spyOn(global, 'window', 'get')
+    windowSpy.mockImplementation(
+        () =>
+            ({
+                ...originalWindow,
+                matchMedia: () => ({
+                    matches: false,
+                    addListener: () => void 0,
+                    removeListener: () => void 0,
+                }),
+            } as any)
+    )
     expect(darkmode((e, k) => {}).removeListeners())
 })
